0f6eb051f8c7417784eb7db1a5ebe12afb0f8c61
Xue <923051761@qq.com>
2024-05-02 15:08:41 +0800

    2024.5.2
    2024.5.2

---
+++ 面试经典150题/接雨水.cpp
@@ -0,0 +1,63 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<stack>
+using namespace std;
+
+
+class Solution {
+public:
+/*
+    int trap(vector<int>& height) {
+        int n = height.size();
+        int res = 0;
+        vector<int> leftMax;
+        leftMax.emplace_back(height[0]);
+        for(int i = 1; i < n;i ++){
+            int tem = max(leftMax[i - 1], height[i]);
+            leftMax.emplace_back(tem);
+        }
+        int rightMax = height[n - 1];
+        for(int i = n - 2; i >= 0; i--){
+            rightMax = max(rightMax, height[i]);
+            if(height[i] < leftMax[i] && height[i] < rightMax){
+                int tem = min(leftMax[i], rightMax) - height[i];
+                res += tem;
+            }
+        }
+        return res;
+    }*/
+
+    int trap(vector<int>& height){
+        int n = height.size();
+        int leftMax = height[0];
+        int rightMax = height[n - 1];
+        int leftInd = 0;
+        int rightInd = n - 1;
+        int res = 0;
+        while(leftInd < rightInd){
+            if(leftMax < rightMax){
+            	res += leftMax - height[leftInd];
+            	leftInd++;
+            	if(leftMax < height[leftInd]){
+            		leftMax = height[leftInd];
+				}
+			}
+			else{
+				res += rightMax - height[rightInd];
+				rightInd--;
+				if(rightMax < height[rightInd]){
+					rightMax = height[rightInd];
+				}
+			}
+        }
+        return res;
+    }
+};
+
+int main(){
+    vector<int> sss = {0,1,0,2,1,0,1,3,2,1,2,1};
+    Solution sol;
+    cout<<sol.trap(sss);
+    return 0;
+}
